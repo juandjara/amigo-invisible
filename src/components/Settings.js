da3c54419392a56435e1030142d1aba05ed1a372
Juan Domínguez <juan.dominguez@dinamicarea.com>
2017-10-18 11:24:11 +0200

    feat: finish account ui

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -1,32 +1,58 @@
 import React, { Component } from 'react';
-import firebase from '../services/firebase'
+import { 
+  login,
+  logout, 
+  register,
+  loginObserver, 
+  updateProfile,
+  getProfile
+} from '../services/firebase'
 import TextField from 'material-ui/TextField';
 import Button from 'material-ui/Button'
 import Tabs, { Tab } from 'material-ui/Tabs';
+import Icon from 'material-ui/Icon'
 
 export default class Settings extends Component {
   state = {
     email: '',
     password: '',
+    name: '',
+    likes: '',
+    dislikes: '',
     tab: 0,
-    user: null
+    user: null,
+    loading: true
   }
   componentDidMount() {
-    firebase.auth().onAuthStateChanged(user => {
-      this.setState({user})
+    loginObserver(user => {
+      this.setState({user, loading: true})
       user ? console.log("Logged in with email ", user.email) : console.log("Logged out")
+      getProfile(user.uid).then(profile => {
+        this.setState({
+          loading: false,
+          name: profile.name || '',
+          likes: profile.likes || '',
+          dislikes: profile.dislikes || ''
+        })
+      })
     })
   }
   onSubmit(ev) {
     ev.preventDefault()
     const {email, password, tab} = this.state
-    let promise;
-    if(tab === 0) {
-      promise = firebase.auth().signInWithEmailAndPassword(email, password)
-    } else {
-      promise = firebase.auth().createUserWithEmailAndPassword(email, password)      
-    }
+    this.setState({loading: true})
+    const promise = tab === 0 ? 
+      login(email, password) : 
+      register(email, password)
+    promise.then(() => this.setState({loading: false}))
     promise.catch(err => console.error(err.message))
+  }
+  updateProfile(ev) {
+    ev.preventDefault()
+    const {user, name, likes, dislikes} = this.state
+    this.setState({loading: true})
+    updateProfile(user.uid, {name, likes, dislikes})
+    .then(() => this.setState({ loading: false }))
   }
   handleChange = name => ev => {
     this.setState({
@@ -44,6 +70,7 @@ export default class Settings extends Component {
       <form style={{padding: '1em'}} onSubmit={ev => this.onSubmit(ev)}>
         <TextField
           fullWidth
+          style={{margin: '1em 0'}} 
           label="Email"
           type="email"
           value={email}
@@ -66,11 +93,6 @@ export default class Settings extends Component {
       </form>
     );
   }
-  renderRegister() {
-    return (
-      <div></div>
-    )
-  }
   renderTabs() {
     return (
       <div>
@@ -84,13 +106,54 @@ export default class Settings extends Component {
       </div>
     )
   }
-  render() {
-    const {user} = this.state
-    return user ? (
+  renderAccount() {
+    const {user, name, likes, dislikes} = this.state
+    return (
       <div>
-        <h2>Bienvenido {user.email}</h2>
-        <Button onClick={() => firebase.auth().signOut()}>Cerrar sesi&oacute;n</Button>
+        <div style={{display: 'flex', flexWrap: 'wrap', alignItems: 'center', margin: '1em'}}>
+          <h2 style={{margin: 0}}>{user.email}</h2>
+          <div style={{flex: 1}}></div>
+          <Button onClick={() => logout()}>
+            <Icon style={{verticalAlign: 'middle', marginRight: 8}}>close</Icon> Cerrar sesi&oacute;n
+          </Button>
+        </div>
+        <form style={{margin: '1em'}} onSubmit={ev => this.updateProfile(ev)}>
+          <TextField 
+            fullWidth
+            style={{margin: '1em 0'}}
+            label="Nombre"
+            value={name}
+            onChange={this.handleChange("name")}
+          />
+          <TextField 
+            fullWidth
+            style={{margin: '1em 0'}}
+            label="Me gusta"
+            value={likes}
+            onChange={this.handleChange("likes")}
+          />
+          <TextField 
+            fullWidth
+            style={{margin: '1em 0'}}
+            label="No me gusta"
+            value={dislikes}
+            onChange={this.handleChange("dislikes")}
+          />
+          <Button type="submit" raised color="primary">
+            Guardar
+          </Button>
+        </form>
       </div>
-    ) : this.renderTabs()
+    )
+  }
+  render() {
+    const {loading, user} = this.state
+    const main = user ? this.renderAccount() : this.renderTabs()
+    return (
+      <main>
+        {loading && (<h3 style={{margin: '1rem'}}>Cargando ...</h3>)}
+        {main}
+      </main>
+    )
   }
 }
--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,4 +3,32 @@ import firebase from 'firebase'
 
 firebase.initializeApp(config)
 
+export const loginObserver = cb => firebase.auth().onAuthStateChanged(cb)
+
+export function login(email, password) {
+  return firebase.auth().signInWithEmailAndPassword(email, password)
+  .then(user => user.uid)
+  .then(getProfile)
+}
+export function getProfile(uid) {
+  return firebase.database()
+    .ref('/users/' + uid)
+    .once('value')
+    .then(snaphost => snaphost.val() || {})
+}
+export function updateProfile(uid, data) {
+  return firebase.database()
+    .ref('/users/' + uid)
+    .set(data)
+}
+export function register(email, password) {
+  return firebase.auth().createUserWithEmailAndPassword(email, password)
+  .then(user => user.uid)
+  .then(getProfile)
+}
+export function logout() {
+  return firebase.auth().signOut()
+}
+
+
 export default firebase
