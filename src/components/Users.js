4a7771aad55f300feff96d3ba386959abefe78d3
Juan Domínguez <juan.dominguez@dinamicarea.com>
2017-10-18 12:55:56 +0200

    feat: user list finished

--- src/components/Users.js
+++ src/components/Users.js
@@ -2,6 +2,14 @@ import React from 'react'
 import { addDBListener } from '../services/firebase'
 import List, { ListItem } from 'material-ui/List';
 import { CircularProgress } from 'material-ui/Progress';
+import Icon from 'material-ui/Icon'
+import styled from 'styled-components'
+
+const ListIcon = styled(Icon)`
+  vertical-align: bottom;
+  margin-right: 10px;
+  color: ${props => props.color};
+`
 
 class Users extends React.Component {
   state = {
@@ -32,8 +40,23 @@ class Users extends React.Component {
         )}
         <List>
           {users.map((user, i) => (
-            <ListItem key={i}>
-               {user.name}
+            <ListItem key={i} style={{border: '1px solid #ccc', margin: '.5em 1em'}}>
+              <div>
+                <p style={{fontSize: 20, marginTop: 0}}>
+                  <ListIcon>person</ListIcon>
+                  <span>{user.name}</span>
+                </p>
+                <p style={{marginTop: 20, marginBottom: 10}}>
+                  <ListIcon color="green">thumb_up</ListIcon> 
+                  <strong>Me gusta:</strong>
+                </p>
+                <div>{user.likes}</div>
+                <p style={{marginTop: 20, marginBottom: 10}}>
+                  <ListIcon color="red">thumb_down</ListIcon>
+                  <strong>No me gusta:</strong>
+                </p>
+                <div>{user.dislikes}</div>                
+              </div>
             </ListItem>
           ))}
         </List>
